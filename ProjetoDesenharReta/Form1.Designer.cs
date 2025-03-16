4d8ad25bb42c927ffe9aed152f92d583f6b1b72c
Joao-Marcelo-B <joaomarcelobatistanarciso@gmail.com>
2025-03-15 23:48:17 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDesenharReta/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ProjetoDesenharReta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdDDA = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdDDA
+            // 
+            this.cmdDDA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdDDA.Location = new System.Drawing.Point(539, 42);
+            this.cmdDDA.Name = "cmdDDA";
+            this.cmdDDA.Size = new System.Drawing.Size(148, 61);
+            this.cmdDDA.TabIndex = 4;
+            this.cmdDDA.Text = "DDA";
+            this.cmdDDA.UseVisualStyleBackColor = true;
+            this.cmdDDA.Click += new System.EventHandler(this.cmdDDA_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 360);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(539, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 61);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Desenha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(539, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 61);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Bresenhan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 488);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmdDDA);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Trabalho de Computação Gráfica";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdDDA;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ProjetoDesenharReta/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoDesenharReta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x1, x2, y1, y2;
+        int c = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            alg_DDA(250, 359 - 50, 100, 359 - 250, bmp);
+
+            alg_DDA(180, 359 - 50, 320, 359 - 50, bmp);
+
+            alg_DDA(80, 359 - 200, 100, 359 - 250, bmp);
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bresenhan(x1, (359 - y1), x2, (359 - y2), bmp);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (c == 0)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                c++;
+            }
+            else
+            {
+                x2 = e.X;
+                y2 = e.Y;
+                c = 0;
+            }
+        }
+
+        private void cmdDDA_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            alg_DDA(x1, (359 - y1), x2, (359-y2), bmp);
+            pictureBox1.Image = bmp;
+           
+        }
+
+        private void bresenhan(int x1, int y1, int x2, int y2, Bitmap bmp)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            int p = 2 * dy - dx;
+
+            bmp.SetPixel(x1, 359 - y1, Color.Black);
+
+            int x = x1;
+            int y = y1;
+            for(x1 = x1 +1; x1 <= x2; x1++)
+            {
+                if(p < 0)
+                {
+                    x++;
+                    bmp.SetPixel(x, 359 - y, Color.Black);
+                    p += 2 * dy;
+                } 
+                else
+                {
+                    x++;
+                    y++;
+                    bmp.SetPixel(x + 1, 359 - (y + 1), Color.Black);
+                    p += 2 * (dy - dx);
+                }
+            }
+        }
+
+       
+        private void alg_DDA(int x1, int y1, int x2, int y2, Bitmap bmp)
+        {
+            int dx = x2 - x1;
+            if (dx < 0)
+            {
+                alg_DDA(x2, y2, x1, y1, bmp);
+                return;
+            }
+
+            int dy = y2 - y1;
+
+            float m = (float)dy / dx;
+
+            bmp.SetPixel(x1, 359 - y1, Color.Black);
+
+            float y = y1;
+            for(x1 = x1+1; x1 <= x2; x1++)
+            {
+                y += m;
+                bmp.SetPixel(x1, 359 - (int)y, Color.Black);
+            }
+        }
+
+              
+
+     
+    }
+}
